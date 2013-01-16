0613ebd6b6a454846569e9405e3c9d659ed18203
Terrance Niechciol <TNiechciol@gmail.com>
2013-01-16 09:58:02 -0500

    Input loaded and parsed before printing output

--- Main.hs
+++ Main.hs
@@ -1,18 +1,18 @@
 module Main where
 
 import System.IO
+import Control.Monad
 import qualified Data.ByteString.Lazy as BL
 import Text.ParserCombinators.Parsec
 
 import Parser
 
 main = do
-    input <- hGetLine stdin
-    case parse parseOp "parseOp" input of
-        Left error -> putStrLn ("Error: " ++ (show error))
-        Right result -> do
-            BL.putStr $ BL.pack result
-    streamEnded <- hIsEOF stdin
-    if streamEnded
-        then return ()
-        else main
+    input <- hGetContents stdin
+    operations <- mapM parseLine (lines input)
+    mapM_ (BL.putStr . BL.pack) operations
+  where
+    parseLine line = 
+        case parse parseOp "parseOp" line of
+            Left parseError -> error ("Error: " ++ (show parseError))
+            Right result -> return result
